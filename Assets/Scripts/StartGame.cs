0faf8b099f396f0d7138ea17ec7e4cd19bc0a161
salingmw <58449342+salingmw@users.noreply.github.com>
2022-10-23 18:06:54 -0400

    Updated Restart Button Control

--- Assets/Scripts/StartGame.cs
+++ Assets/Scripts/StartGame.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,7 +6,75 @@ using UnityEngine.SceneManagement;
 
 public class StartGame : MonoBehaviour
 {
+    GameObject[] pauseObjects;
     public string levelName;
+
+    void Start()
+    {
+        Time.timeScale = 1;
+        pauseObjects = GameObject.FindGameObjectsWithTag("ShowOnPause");
+        hidePaused();
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            if(Time.timeScale == 1)
+            {
+                Time.timeScale = 0;
+                showPaused();
+            } else if(Time.timeScale == 0)
+            {
+                Time.timeScale = 1;
+                hidePaused();
+            }
+        }
+    }
+
+    public void Reload()
+    {
+        Scene scene = SceneManager.GetActiveScene(); 
+        SceneManager.LoadScene(scene.name);
+        //levelName = SceneManager.GetActiveScene().ToString();
+        //SceneManager.LoadScene(levelName);
+    }
+
+    public void pauseControl()
+    {
+        if (Time.timeScale == 1)
+        {
+            Time.timeScale = 0;
+            showPaused();
+        }
+        else if (Time.timeScale == 0)
+        {
+            Time.timeScale = 1;
+            hidePaused();
+        }
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("StartScreen");
+    }
+
+    private void showPaused()
+    {
+        foreach (GameObject g in pauseObjects)
+        {
+            g.SetActive(true);
+        }
+    }
+
+    private void hidePaused()
+    {
+        foreach(GameObject g in pauseObjects)
+        {
+            g.SetActive(false);
+        }
+    }
+
 
     public void LoadLevel()
     {

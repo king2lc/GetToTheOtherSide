25552329fef49ec0c0b750d978d4977671ad297a
Odogwufu <94500778+Odogwufu@users.noreply.github.com>
2023-03-09 03:11:25 -0500

    added skins and change next

--- Assets/Scripts/Characters.cs
+++ Assets/Scripts/Characters.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Characters : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public GameObject[] skins;
+    public int selectedCharacter;
+    private void Awake(){
+        selectedCharacter = PlayerPrefs.GetInt("SelectedCharacter",0);
+        foreach(GameObject player in skins )
+        player.SetActive(false);
+        skins[selectedCharacter].SetActive(true);
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void ChangeNext(){
+        skins[selectedCharacter].SetActive(false);
+        selectedCharacter++;
+        if(selectedCharacter == skins.Length)
+            selectedCharacter = 0;
+        skins[selectedCharacter].SetActive(true);
+        PlayerPrefs.SetInt("SelectedCharacter",selectedCharacter);
     }
+    public void ChangePrevious(){
+        skins[selectedCharacter].SetActive(false);
+        selectedCharacter++;
+        if(selectedCharacter == skins.Length)
+            selectedCharacter = 0;
+        skins[selectedCharacter].SetActive(true);
+        PlayerPrefs.SetInt("SelectedCharacter",selectedCharacter);
+    }
+   
 }

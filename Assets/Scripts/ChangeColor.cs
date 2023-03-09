2daea065194dde3ec601462cb6a38774ae083882
Odogwufu <94500778+Odogwufu@users.noreply.github.com>
2023-03-09 02:58:57 -0500

    added color change to customization.

---
+++ Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+    private Renderer rend;
+
+    [SerializeField]
+    private Color colorToTurnTo = Color.white;
+
+    private void Start(){
+        rend = GetComponent<Renderer>();
+        rend.material.color = colorToTurnTo;
+    }
+ 
+}

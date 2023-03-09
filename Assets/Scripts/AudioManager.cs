0f9e2b720da68c3b5022a891056dc6a6215b0e04
Odogwufu <94500778+Odogwufu@users.noreply.github.com>
2023-03-09 01:52:39 -0500

    volume settings

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class AudioManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public AudioMixer theMixer;
+    void Start(){
+        if(PlayerPrefs.HasKey("MasterVol")){
+            theMixer.SetFloat("MasterVol", PlayerPrefs.GetFloat("MasterVol"));
+        }
+        if(PlayerPrefs.HasKey("MusicVol")){
+            theMixer.SetFloat("MusicVol", PlayerPrefs.GetFloat("MusicVol"));
+        }
+        if(PlayerPrefs.HasKey("SFXrVol")){
+            theMixer.SetFloat("SFXVol", PlayerPrefs.GetFloat("SFXVol"));
+        }
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 }
